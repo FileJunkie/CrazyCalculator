507ab8c80ad7f16868e1a1c9414e7879b6f887ea
Ilya 'FileJunkie' Ershov <filejunkie@gmail.com>
2010-11-15 16:26:52 +0300

    Long substraction added

--- src/name/filejunkie/CrazyCalculator/Z9Number.java
+++ src/name/filejunkie/CrazyCalculator/Z9Number.java
@@ -42,6 +42,22 @@ public class Z9Number {
 		
 		return result;
 	}
+	
+	@SuppressWarnings("unchecked")
+	public Set<Z9Number> sub(Z9Number b){
+		ArrayList<Integer> numb1 = (ArrayList<Integer>)this.number.clone();
+		ArrayList<Integer> numb2 = (ArrayList<Integer>)b.number.clone();
+			
+		Set<ArrayList<Integer>> results = Z9Number.sub(numb1, numb2);
+		Set<Z9Number> result = new HashSet<Z9Number>();
+		for(ArrayList<Integer> res: results){
+			Z9Number r = new Z9Number(0);
+			r.number = res;
+			result.add(r);
+		}
+		
+		return result;
+	}
 
 	@SuppressWarnings("unchecked")
 	private static Set<ArrayList<Integer>> add(ArrayList<Integer> number1, ArrayList<Integer> number2){
@@ -96,6 +112,59 @@ public class Z9Number {
 		return resNumbers;
 	}
 
+	@SuppressWarnings("unchecked")
+	private static Set<ArrayList<Integer>> sub(ArrayList<Integer> number1, ArrayList<Integer> number2){
+		while(number1.size() > number2.size()){
+			number2.add(0);
+		}
+		while(number1.size() < number2.size()){
+			number1.add(0);
+		}
+		
+		Integer digit1 = number1.get(0);
+		Integer digit2 = number2.get(0);
+		Set<Integer> resDigits = Z9Digit.sub(digit1, digit2);
+		Set<ArrayList<Integer>> resNumbers = new HashSet<ArrayList<Integer>>();
+		boolean overflow = false;
+		
+		for(int resDigit: resDigits){
+			overflow = (resDigit > digit1 - digit2) || (digit1 != 0 && digit2 != 0 && resDigit == digit1 + digit2);
+			ArrayList<Integer> numb1 = (ArrayList<Integer>)number1.clone();
+			ArrayList<Integer> numb2 = (ArrayList<Integer>)number2.clone();
+			numb1.remove(0);
+			numb2.remove(0);
+			if(numb1.size() != 0){
+				Set<ArrayList<Integer>> resultsHigh = Z9Number.sub(numb1, numb2);
+				if(overflow){
+					Set<ArrayList<Integer>> tmpSet = resultsHigh;
+					resultsHigh = new HashSet<ArrayList<Integer>>();
+					ArrayList<Integer> tmpArr = new ArrayList<Integer>();
+					tmpArr.add(1);
+					for(int i = 1; i < numb1.size(); i++){
+						tmpArr.add(0);
+					}
+					for(ArrayList<Integer> keyArr: tmpSet){
+						resultsHigh.addAll(Z9Number.sub(keyArr, tmpArr));
+					}
+				}
+				for(ArrayList<Integer> resHigh: resultsHigh){
+					resHigh.add(0, resDigit);
+					resNumbers.add(resHigh);
+				}
+			}
+			else{
+				ArrayList<Integer> tmp = new ArrayList<Integer>();
+				tmp.add(resDigit);
+				if(overflow){
+					tmp.add(1);
+				}
+				resNumbers.add(tmp);
+			}
+		}
+		
+		return resNumbers;
+	}
+	
 	@Override
 	public String toString() {
 		String res = "";
